bf0cc5e87327e170fd3812e736a21b999bc20ec9
MadeleineSmith <madeleinesmith1995@gmail.com>
2017-01-16 16:53:26 +0000

    fix adding two items with same name

--- demo.js
+++ demo.js
@@ -1,7 +1,69 @@
+// function baz() {
+//     // call-stack is: `baz`
+//     // so, our call-site is in the global scope
+//
+//     console.log( "baz" );
+//     bar(); // <-- call-site for `bar`
+// }
+//
+// function bar() {
+//     // call-stack is: `baz` -> `bar`
+//     // so, our call-site is in `baz`
+//
+//     console.log( "bar" );
+//     foo(); // <-- call-site for `foo`
+// }
+//
+// function foo() {
+//     debugger;
+//     // call-stack is: `baz` -> `bar` -> `foo`
+//     // so, our call-site is in `bar`
+//
+//     console.log( "foo" );
+// }
+//
+// baz(); // <-- call-site for `baz`
+
+
+// function foo() {
+//     console.log( this.a );
+// }
+//
+// var obj = {
+//     a: 2,
+//     foo: foo
+// };
+//
+// obj.foo(); // 2
+
+// function foo() {
+//     console.log( this.a );
+// }
+//
+// var obj2 = {
+//     a: 42,
+//     foo: foo
+// };
+//
+// var obj1 = {
+//     a: 2,
+//     obj2: obj2
+// };
+//
+// obj1.obj2.foo(); // 42
+
+
 function foo() {
     console.log( this.a );
 }
 
-var a = 2;
+var obj = {
+    a: 2,
+    foo: foo
+};
 
-foo(); // 2
+var bar = obj.foo; // function reference/alias!
+
+var a = "oops, global"; // `a` also property on global object
+
+bar(); // "oops, global"
--- modulePattern.js
+++ modulePattern.js
@@ -1,6 +1,21 @@
 function Basket() {
     var basket = [];
 
+    /**
+     *
+     * @param item
+     * @return true if item found in basket array, false otherwise
+     */
+    function checkPresenceOfItem(item) {
+        var indexNo;
+
+        for (indexNo in basket) {
+
+            if (basket[indexNo].label === item.label) {
+                return indexNo;
+            }
+        }
+    }
 
     /**
      * Adds the item to the basket, saving it locally
@@ -8,105 +23,108 @@ function Basket() {
      * @param {Object} item
      * @param {Number} quantity
      */
-    function addToBasket( item, quantity ) {
-        item.amount = quantity;
+    function addToBasket(item, quantity) {
+        var indexNo = checkPresenceOfItem(item);
 
-        basket.push(item);
+        if (indexNo >= 0) {
+            basket[indexNo].amount += quantity;
+        } else {
+            item.amount = quantity;
+            basket.push(item);
+        }
 
-        //@TODO remove:
-        console.log(basket);
 
     }
+        /**
+         * Removes the specified item (using its label),
+         * or - if the quantity is >= will just subtract the required quantity and return it
+         *
+         * @param {Object} item
+         * @param {Number} quantity
+         *
+         * @returns {Mixed}
+         * true if the item has been removed,
+         * false if no object has been removed, or the number of remaining items.
+         */
+        function removeFromBasket(item, quantity) {
+            var indexNo2;
 
-    /**
-     * Removes the specified item (using its label),
-     * or - if the quantity is >= will just subtract the required quantity and return it
-     *
-     * @param {Object} item
-     * @param {Number} quantity
-     *
-     * @returns {Mixed}
-     * true if the item has been removed,
-     * false if no object has been removed, or the number of remaining items.
-     */
-    function removeFromBasket( item, quantity ) {
-        var indexNo;
+            for (indexNo2 in basket) {
+                if (basket[indexNo2].label === item.label) {
+                    if (basket[indexNo2].amount >= quantity && quantity > 0) {
 
-        for (indexNo in basket) {
-            if (basket[indexNo].label === item.label) {
-                if (basket[indexNo].amount >= quantity && quantity > 0 ) {
+                        basket[indexNo2].amount -= quantity;
 
-                    basket[indexNo].amount -= quantity;
+                        //@TODO remove line later
+                        console.log(basket);
 
-                    //@TODO remove line later
-                    console.log(basket);
+                        if (basket[indexNo2].amount === 0) {
+                            basket.splice(parseInt(indexNo2), 1);
+                            return true;
+                        }
 
-                    if (basket[indexNo].amount === 0) {
-                        basket.splice(parseInt(indexNo), 1);
-                        return true;
+                        return basket[indexNo2].amount;
+                    }
+                    else {
+                        return false;
                     }
 
-                    return basket[indexNo].amount;
-                }
-                else {
-                    return false;
-                }
 
-
+                }
             }
+
         }
 
-    }
+        /**
+         * calculates the total of the items in the basket
+         *
+         * @returns {Number} the total
+         */
+        function calculateTotal() {
+            var basketTotal = 0;
+            var indexNo3;
 
-    /**
-     * calculates the total of the items in the basket
-     *
-     * @returns {Number} the total
-     */
-    function calculateTotal() {
-        var basketTotal = 0;
-        var indexNo;
+            for (indexNo3 in basket) {
+                basketTotal += basket[indexNo3].cost * basket[indexNo3].amount;
+            }
+            return basketTotal;
 
-        for (indexNo in basket) {
-            basketTotal += basket[indexNo].cost * basket[indexNo].amount;
-        }
-        return basketTotal;
-
-    }
-
-    /**
-     * pretty prints the item in the following format:
-     * <Label> (<quantity>/£<cost>) total: £<total>
-     *
-     * @returns {String}
-     */
-    function prettyPrintItem( item ) {
-
-        return item.label + ' (' + item.amount + '/£' + item.cost + ')' + ' total: £' + (item.amount * item.cost).toFixed(2);
-
-    }
-
-    /**
-     * Pretty prints each item in the basket and the total at the end
-     */
-    function prettyPrintBasket() {
-        var indexNo;
-
-        for (indexNo in basket) {
-            console.log(prettyPrintItem(basket[indexNo]));
         }
 
-        console.log('--');
-        console.log('Total: £' + calculateTotal().toFixed(2));
+        /**
+         * pretty prints the item in the following format:
+         * <Label> (<quantity>/£<cost>) total: £<total>
+         *
+         * @returns {String}
+         */
+        function prettyPrintItem(item) {
+
+            return item.label + ' (' + item.amount + '/£' + item.cost + ')' + ' total: £' + (item.amount * item.cost).toFixed(2);
+
+        }
+
+        /**
+         * Pretty prints each item in the basket and the total at the end
+         */
+        function prettyPrintBasket() {
+            var indexNo4;
+
+            for (indexNo4 in basket) {
+                console.log(prettyPrintItem(basket[indexNo4]));
+            }
+
+            console.log('--');
+            console.log('Total: £' + calculateTotal().toFixed(2));
+        }
+
+        return {
+            addToBasket: addToBasket,
+            removeFromBasket: removeFromBasket,
+            calculateTotal: calculateTotal,
+            prettyPrintBasket: prettyPrintBasket,
+        }
     }
 
-    return {
-        addToBasket: addToBasket,
-        removeFromBasket: removeFromBasket,
-        calculateTotal: calculateTotal,
-        prettyPrintBasket: prettyPrintBasket,
-    }
-}
 
 var apple = {
     cost : 0.45,
@@ -146,10 +164,12 @@ var cereal = {
 
 var randomBasket = Basket();
 
-randomBasket.addToBasket(apple, 5);
-randomBasket.addToBasket(banana, 10);
+randomBasket.addToBasket(apple, 1);
+randomBasket.addToBasket(cereal, 5);
+randomBasket.addToBasket(cereal, 10);
+randomBasket.addToBasket(banana, 34);
 
-randomBasket.removeFromBasket(banana,2);
+// randomBasket.removeFromBasket(banana,2);
 
 randomBasket.calculateTotal();
 
